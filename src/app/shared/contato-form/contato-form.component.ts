31ffcb585b9c68feb0de7d7cd57c641e30ad25bf
Rafael Lima <srafalimacosta@gmail.com>
2023-09-04 13:56:51 -0300

    <style> contato form especial

--- src/app/shared/contato-form/contato-form.component.ts
+++ src/app/shared/contato-form/contato-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contato-form',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./contato-form.component.css']
 })
 export class ContatoFormComponent {
-
+  @Input() versaoEspecial: boolean = false;
 }
